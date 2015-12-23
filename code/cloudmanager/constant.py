36989c083b9de5daef28e97c36919f73ace48190
back1860 <back1860@outlook.com>
2015-12-23 17:54:43 +0800

    cloud_manager_Austin

---
+++ code/cloudmanager/constant.py
@@ -0,0 +1,96 @@
+# -*- coding:utf-8 -*-
+__author__ = 'q00222219@huawei'
+
+import os
+
+
+class PublicConstant(object):
+    SCRIPTS_DIR = os.path.join("/home/hybrid_cloud",
+                               "scripts", "public")
+    ADD_HOST_ADDRESS_SCRIPT = "add_host_address.sh"
+    MODIFY_DNS_SERVER_ADDRESS = "modify_dns_server_address.sh"
+
+
+class VpnConstant(object):
+    VPN_ROOT = "root"
+    VPN_ROOT_PWD = "Galax0088"
+    AWS_VPN_ROOT = "root"
+    AWS_VPN_ROOT_PWD = "Galax0088"
+    REMOTE_SCRIPTS_DIR = "/root/ipsec_config/"
+    LIST_TUNNEL_SCRIPT = "list_tunnel.sh"
+    ADD_TUNNEL_SCRIPT = "add_tunnel_ex.sh"
+    REMOVE_TUNNEL_SCRIPT = "remove_tunnel.sh"
+
+
+class Cascaded(object):
+    ROOT = "root"
+    ROOT_PWD = "cnp200@HW"
+    REMOTE_SCRIPTS_DIR = "/root/cloud_manager/cascaded/"
+    MODIFY_PROXY_SCRIPT = "modify_proxy.sh"
+    MODIFY_CASCADED_SCRIPT = "modify_cascaded_domain.sh"
+    MODIFY_CASCADED_SCRIPT_PY = "cascaded_handler.py"
+    CASCADED_ADD_ROUTE_SCRIPT = "cascaded_add_route.sh"
+    ADD_ROUTE_SCRIPT = "add_route.sh"
+    ADD_API_ROUTE_SCRIPT = "cascaded_add_api_route.sh"
+    CREATE_ENV = "create_env.sh"
+    CONFIG_CINDER_SCRIPT = "config_storage.sh"
+    CONFIG_CINDER_BACKUP_SCRIPT = "ceph_patch/backup_az.py"
+    UPDATE_NETWORK_VLAN_SCRIPT = "update_network_vlan.sh"
+    UPDATE_L3_AGENT_SCRIPT = "update_neutron_l3_agent.sh"
+
+
+class Cascading(object):
+    ROOT = "root"
+    ROOT_PWD = "Huawei@CLOUD8!"
+    REMOTE_SCRIPTS_DIR = "/home/hybrid_cloud/scripts/cascading/"
+    CHECK_PROXY_SCRIPT = "check_free_proxy.sh"
+    ADD_VPN_ROUTE_SCRIPT = "add_vpn_route.sh"
+    KEYSTONE_ENDPOINT_SCRIPT = "create_keystone_endpoint.sh"
+    ENABLE_OPENSTACK_SERVICE = "enable_openstack_service.sh"
+
+
+class PatchesConstant(object):
+    REMOTE_SCRIPTS_DIR = "/home/hybrid_cloud/scripts/patches/"
+    PATCH_LUNCH_DIR = "/home/hybrid_cloud/scripts/patches/patches_tool"
+    CONFIG_PATCHES_SCRIPT = "config_patches_tool_config.sh"
+    CONFIG_AWS_SCRIPT = "config_aws.sh"
+    CONFIG_ROUTE_SCRIPT = "config_add_route.sh"
+
+
+class RemoveConstant(object):
+    REMOTE_SCRIPTS_DIR = "/home/hybrid_cloud/scripts/remove/"
+    REMOVE_KEYSTONE_SCRIPT = "remove_keystone_endpoint.sh"
+    REMOVE_PROXY_SCRIPT = "remove_proxy_host.sh"
+    REMOVE_AGGREGATE_SCRIPT = "remove_aggregate.sh"
+    REMOVE_CINDER_SERVICE_SCRIPT = "remove_cinder_service.sh"
+    REMOVE_NEUTRON_AGENT_SCRIPT = "remove_neutron_agent.sh"
+
+
+class CephConstant(object):
+    USER = "ceph"
+    PWD = "ceph@huawei"
+    CEPH_INSTALL_SCRIPT = "/home/ceph/config-sh/install.sh"
+
+
+class Proxy(object):
+    USER = "root"
+    PWD = "Huawei@CLOUD8!"
+    LOCAL_NEUTRON_PROXY_DIR = "/home/hybrid_cloud/code/proxy/neutron_proxy"
+    REMOTE_NEUTRON_PROXY_DIR = "/usr/lib64/python/site-packages/neutron/agent"
+    L3_PROXY_CODE = "l3_proxy.py"
+
+
+class FusionsphereConstant(object):
+    ROOT = "root"
+    ROOT_PWD = "Huawei@CLOUD8!"
+
+
+class VcloudConstant(object):
+    ROOT = "root"
+    ROOT_PWD = "Huawei@CLOUD8!"
+
+
+class AwsConstant(object):
+    ROOT = "root"
+    ROOT_PWD = "cnp200@HW"
+
--- scripts/unused/add_host_address.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-#2015.8.18 test ok
-address=${1}
-cps template-params-update --parameter address=${address} --service dns dns-server
-cps commit 
